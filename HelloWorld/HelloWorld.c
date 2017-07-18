b7a5608cb25eb7278db737b76dbd96f276d3c547
Michael Goberling <michael.goberling@gmail.com>
2017-07-17 21:14:43 -0500

    Hello world test.

---
+++ HelloWorld/HelloWorld.c
@@ -0,0 +1,21 @@
+/*
+ * HelloWorld.c
+ *
+ *  Created on: Jul 17, 2017
+ *      Author: Michael Goberling
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	char a[] = "Hello ";
+	char b[] = "World \n";
+
+	printf("%s %s", a, b);
+
+	return 0;
+}
+
+
